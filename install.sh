95b8303d372851490a70c60b5593573928ec5da6
Jim Evans <jim@vheavy.com>
2023-04-15 21:41:47 +0100

    Changed mqtt server added install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 sudo apt-get install mosquitto npm nodejs
 npm install
 node ./doorserver.js
+echo listener 1883 | sudo tee -a /etc/mosquitto/mosquitto.conf
+
+echo allow_anonymous true | sudo tee -a /etc/mosquitto/mosquitto.conf
+
+ 
